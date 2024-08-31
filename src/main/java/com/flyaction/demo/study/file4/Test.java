f4c4117932264e9c71c111e57c8e9fe2a7926b96
flyaction <823572373@qq.com>
2024-08-31 16:15:31 +0800

    feat:序列化-240831

--- src/main/java/com/flyaction/demo/study/file4/Test.java
+++ src/main/java/com/flyaction/demo/study/file4/Test.java
@@ -3,10 +3,19 @@ package com.flyaction.demo.study.file4;
 
 import com.flyaction.demo.study.file4.entity.User;
 
-public class Test {
-    public static void main(String[] args) {
-        User user = new User(1,"张三",22);
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 
+public class Test {
+    public static void main(String[] args) throws Exception {
+        User user = new User(1,"张三",22);
+        OutputStream outputStream = new FileOutputStream("E:\\javaProject\\java_everyday\\src\\main\\java\\com\\flyaction\\demo\\study\\file4\\test5.txt");
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+        objectOutputStream.writeObject(user);
+        objectOutputStream.flush();
+        objectOutputStream.close();
+        outputStream.close();
 
     }
 }
