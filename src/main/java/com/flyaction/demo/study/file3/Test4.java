02e7dd0d8ed2388a2530f05021116edc1b3cb214
action <xingdong1117@126.com>
2024-08-31 10:53:57 +0800

    feat:字节输出缓冲流-240831

--- src/main/java/com/flyaction/demo/study/file3/Test4.java
+++ src/main/java/com/flyaction/demo/study/file3/Test4.java
@@ -1,22 +1,18 @@
 package com.flyaction.demo.study.file3;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.Reader;
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class Test4 {
     public static void main(String[] args) throws Exception {
-        Reader reader = new FileReader("/Users/action/code/java_aop/src/main/java/com/flyaction/demo/study/file3/test.txt");
-        BufferedReader bufferedReader = new BufferedReader(reader);
-        char[] chars = new char[1024];
-        int length = bufferedReader.read(chars);
-        System.out.println(length);
-        for (char aChar:chars){
-            System.out.println(aChar);
-        }
-        bufferedReader.close();
-        reader.close();
-
+        OutputStream outputStream = new FileOutputStream("/Users/action/code/java_aop/src/main/java/com/flyaction/demo/study/file3/test4.txt");
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(outputStream);
+        String str = "Java是一门面向对象的编程语言，不仅吸收了C++语言的各种优点，还摒弃了C++里难以理解的多继承、指针等概念，因此Java语言具有功能强大和简单易用两个特征。Java语言作为静态面向对象编程语言的代表，极好地实现了面向对象理论，允许程序员以优雅的思维方式进行复杂的编程。";
+        byte[] bytes = str.getBytes();
+        bufferedOutputStream.write(bytes);
+        bufferedOutputStream.flush();
+        bufferedOutputStream.close();
 
     }
 }
