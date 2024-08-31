1c76d953a18b4d247f9fb4545175d38d6e565557
action <xingdong1117@126.com>
2024-08-31 12:51:13 +0800

    feat:序列化-240831

---
+++ src/main/java/com/flyaction/demo/study/file4/Test.java
@@ -0,0 +1,12 @@
+package com.flyaction.demo.study.file4;
+
+
+import com.flyaction.demo.study.file4.entity.User;
+
+public class Test {
+    public static void main(String[] args) {
+        User user = new User(1,"张三",22);
+
+
+    }
+}
---
+++ src/main/java/com/flyaction/demo/study/file4/entity/User.java
@@ -0,0 +1,48 @@
+package com.flyaction.demo.study.file4.entity;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public Integer id;
+    public String name;
+    public Integer age;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public User(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
